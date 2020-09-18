254f97e1238708f81bf45214c3b12fbd25303609
Ken Lane <kenlane33@gmail.com>
2020-09-18 18:10:55 -0400

    Varied grey counties, score RYG, & stroked highlight.

--- src/helpers/colorGen.js
+++ src/helpers/colorGen.js
@@ -1,4 +1,39 @@
 
+export const scoreToColor = (score, sat=100, lum=50, start=0, end=150) => {
+  if ( (score > 1.0) || (score < 0.0) )
+  {
+    console.error('ERROR: score out of 0.0 to 1.0 range in scoreToColor()')
+    return 'black'
+  }
+  const hue = ((1.0-score) * (start - end))+end;
+  return 'hsl(' + hue + ','+sat+'%,'+lum+'%)';
+}
+
+export const hashStrToGrey = (s) => {
+  return scoreToGrey( (hashStrToScore(s) * 0.2 ) +.4 ) 
+}
+
+export const scoreToGrey = (score, start=0, end=150) => {
+  if ( (score > 1.0) || (score < 0.0) )
+  {
+    console.error('ERROR: score out of 0.0 to 1.0 range in scoreToColor()')
+    return 'black'
+  }
+  const lum = ((1.0-score) * (start - end))+end;
+  return 'hsl(' + 0 + ','+0+'%,'+lum+'%)';
+}
+
+export const hashStrToScore = (s) => {
+  var hash = 0, i;
+  const maxInt = (Math.pow(2,32)-1)//
+  for (i = 0; i < s.length; i++) {
+    hash  = ((hash << 5) - hash) + s.charCodeAt(i);
+    hash |= 0; // Convert to 32bit integer
+  }
+  const ret = ((hash+0.0) / (maxInt + 0.0) ) +0.5
+  console.log(ret)
+  return ret;
+}
 
 export const stringToColor = (str) => {
   var hash = 0;
@@ -12,6 +47,7 @@ export const stringToColor = (str) => {
   }
   return color;
 }
+
 export const stringToGrey = (str) => {
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
--- src/us_states/StateMap.js
+++ src/us_states/StateMap.js
@@ -1,17 +1,37 @@
 import React from "react"
-import {stringToColor, stringToGrey} from './../helpers/colorGen'
+import {hashStrToGrey, scoreToColor, stringToColor, stringToGrey} from './../helpers/colorGen'
 
+const overrideMaybe = (key, lookup, defaultVal) => {
+  return ( Object.keys(lookup).includes( key ) ) ? lookup[key] : defaultVal
+}
+const rndScore = () => Math.random()
+
+//----------------------/////////----------------------
 export default function StateMap( props ) {
   const stl = {fill:'#d0d0d0',strokeWidth:'.17829',stroke:'blue'}
   const stateNm = props.stateNm || '<State>'
+  const fills = {
+    Ashe:scoreToColor( rndScore() ), 
+    Buncombe:scoreToColor( rndScore() ), 
+    Gates:scoreToColor( rndScore() ), 
+    Mecklenburg:scoreToColor( rndScore() ), 
+    'New Hanover':scoreToColor( rndScore() ), 
+    Robeson:scoreToColor( rndScore() ),
+    Wake:scoreToColor( rndScore() ),
+  }
+  const highlights = {Onslow:'red'}
   return (
     <svg viewBox="0 0 989.98 627.07" version="1.0">
       {props.children}
       <g id="counties" transform="translate(-3850 -2640) rotate(10) scale(5.8)" style={stl}>
         {props.counties.map( x=>{
           const displayNm = x.name + ', ' + stateNm
-          const countyColor = stringToGrey(displayNm+'x') //stringToColor(displayNm+'x')
-          const stlC = {fill:countyColor}
+          // const countyColor = stringToGrey(displayNm+'x') //stringToColor(displayNm+'x')
+          const countyColor = hashStrToGrey(x.name)
+          const fillCol = overrideMaybe( x.name, fills, countyColor )//( Object.keys(countyScores).includes( x.name ) ) ? countyScores[x.name] : countyColor
+          const strokeCol = overrideMaybe( x.name, highlights, 'black' )
+          const strokeWidth = (strokeCol==='black') ? '' : '1'
+          const stlC = {fill:fillCol, stroke:strokeCol, strokeWidth: strokeWidth}
           return (
             <path
               className='svg-path'
