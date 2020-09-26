a94b04a8505ec59d1391ee6946e1c11d359e3f27
Ken Lane <kenlane33@gmail.com>
2020-09-26 11:06:18 -0400

    Scores from top w USA

--- src/components/maps/fakeColorsHelp.js
+++ src/components/maps/fakeColorsHelp.js
@@ -3,6 +3,7 @@ import {scoreToColor, hashStrToGrey} from './../../helpers/colorGen'
 const rndScore      = ()=>Math.random()
 const rndScoreColor = ()=>scoreToColor( rndScore() )
 
+//-----------////////////////--------------------------
 export const fakeFillColorFn = (partNm,enabledList) => {
   const inList = (enabledList.includes(partNm))
   return(
--- src/components/maps_of_regions/MapJustState_NC.js
+++ src/components/maps_of_regions/MapJustState_NC.js
@@ -2,6 +2,7 @@ import React from "react"
 import countiesByState_NcOnly from '../../data/map_data/state_nc_only/countiesByState_NcOnly'
 import MapDrilldown from '../maps/MapDrilldown'
 import {fakeFillColorFn} from '../maps/fakeColorsHelp'
+// fakeFillColorFn = (partNm,enabledList)=>'red'
 
 //----------------------/////////---------------------
 export default function MapJustState_NC(props) {
