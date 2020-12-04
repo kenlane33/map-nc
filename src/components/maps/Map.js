4e133ad9a2585cfeb6c89bc4d2e84070969dcecf
Ken Lane <kenlane33@gmail.com>
2020-12-04 11:01:40 -0500

    initial commit

--- src/components/maps/Map.js
+++ src/components/maps/Map.js
@@ -1,46 +1,42 @@
-import React from "react"
-import MapSvg from './MapSvg'
-import {getPropsByRegion} from '../../helpers/regionsParts'
+import React from "react";
+import MapSvg from "./MapSvg";
+import { getPropsByRegion } from "../../helpers/regionsParts";
 
 //import {partsByRegion, propsByRegion, regionAbbreviations} from '../data/usa_counties_by_state'
 // import {getPropsByRegion} from '../../helpers/regionsParts'
 
 //----------------------/////////---------------------
-export default function Map( props ) {
-
+export default function Map(props) {
   const {
     regionName,
     doPick,
-    region: {
-      propsByRegion,
-      partsByRegion,
-    },
-  } = props
+    region: { propsByRegion, partsByRegion }
+  } = props;
 
   // Select properties & parts from region dataset
-  const regionProps = getPropsByRegion(regionName, propsByRegion, partsByRegion)
-  const parts = partsByRegion[regionName]()
+  const regionProps = getPropsByRegion(
+    regionName,
+    propsByRegion,
+    partsByRegion
+  );
+  const parts = partsByRegion(regionName);
 
   //----///////////-----------------
-  const doPickHere = (p,r)=>{
-    doPick && doPick( p, r )
-  }
+  const doPickHere = (p, r) => {
+    doPick && doPick(p, r);
+  };
 
   return (
-
     <MapSvg
       key={props.regionName}
       {...props}
-
       parts={parts}
       regionProps={regionProps}
-
       doClickEnabledPart={doPickHere}
       doClickDisabledPart={doPickHere}
       highlights={{}}
       enabledParts={[]}
       blockClicks={true}
     />
-
-  )
+  );
 }
--- src/helpers/regionsParts.js
+++ src/helpers/regionsParts.js
@@ -1,20 +1,20 @@
-import {samplePercent} from './arrayHelp'
+import { samplePercent } from "./arrayHelp";
 
 //-----------///////////////---------------------
-export const fakeRegionProps = (regionName, propsByRegion, partsByRegion)=> {
-  const partNames = partsByRegion[regionName]().map(x=>x.name)
-  const percentToSample = ((Math.random()*0.1) + 0.1) // 10% to 20%
-  return ({
-    viewBox:"0 0 980 600", 
-    transform:"translate(0 0.1) scale(1)",
-    enabled: samplePercent( partNames, percentToSample ),
-  })
-}
+export const fakeRegionProps = (regionName, propsByRegion, partsByRegion) => {
+  const partNames = partsByRegion(regionName).map((x) => x.name);
+  const percentToSample = Math.random() * 0.1 + 0.1; // 10% to 20%
+  return {
+    viewBox: "0 0 980 600",
+    transform: "translate(0 0.1) scale(1)",
+    enabled: samplePercent(partNames, percentToSample)
+  };
+};
 //-----------///////////////---------------------
-export const getPropsByRegion = (regionName, propsByRegion, partsByRegion)=> {
-  const r = propsByRegion[regionName]
-  if (r) return r
+export const getPropsByRegion = (regionName, propsByRegion, partsByRegion) => {
+  const r = propsByRegion[regionName];
+  if (r) return r;
   else {
-    return fakeRegionProps(regionName, propsByRegion, partsByRegion)
+    return fakeRegionProps(regionName, propsByRegion, partsByRegion);
   }
-}
+};
